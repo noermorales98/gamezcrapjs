aea1e5d624b235c28faadb901a5f3c195f8570d3
MendozaServin <carimrafa@gmail.com>
2023-01-30 19:16:53 -0500

    Cambios Insignificantes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,6 @@
 export function Login() {
   return (
-    <div className="container text-white">Login</div>
+    <div className="container position-absolute top-50 start-50 bg-danger text-white" style={{transform:"translate(-50%, -50%)"}}>LOGIN PAGE</div>
   )
 }
 
