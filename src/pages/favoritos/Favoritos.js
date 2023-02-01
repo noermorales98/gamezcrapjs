750b78b0134c74f4306c01cebe4470a1fdc3bfc7
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-02-01 17:18:45 -0500

    add ajustes, favoritos

--- src/pages/favoritos/Favoritos.js
+++ src/pages/favoritos/Favoritos.js
@@ -1,11 +1,21 @@
+import Card from './Card';
 import './Favoritos.css'
 
 function Favoritos() {
-    return (
-      <div>
-        <h1>Favoritos</h1>
+  return (
+    <div className="container">
+      <div className='favoritos__container'>
+        <h1 className='text-white'>Mis favoritos</h1>
+        <div className='favoritos__cards'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default Favoritos;
