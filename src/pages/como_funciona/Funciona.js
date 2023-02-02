a27173841e30c78444d98496c9ff574f4e3f825a
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-02-02 12:24:21 -0500

    cambios

--- src/pages/como_funciona/Funciona.js
+++ src/pages/como_funciona/Funciona.js
@@ -2,18 +2,47 @@ import './Funciona.css'
 export function Funciona() {
     return (
         <div className="text-white container funciona__container">
-            <div className="funciona__icon">
-                <i className="funciona__icon-svg">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-question" viewBox="0 0 16 16">
-                        <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
-                    </svg>
-                </i>
+            <h1 className="text-center">Fácil de comprender</h1>
+            <div className="d-flex">
+                <div className="col funciona__col">
+                    <div className="funciona__icon">
+                        <div className="funciona__icon-svg">
+                            <i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" style={{ fill: "rgba(255, 255, 255, 1)" }}><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path><path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path></svg>
+                            </i>
+                        </div>
+
+                    </div>
+                    <h2>Escribe en la barra de busqueda tu juego</h2>
+                </div>
+                <div className="col funciona__col">
+                    <div className="funciona__icon">
+                        <div className="funciona__icon-svg">
+                            <i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" style={{fill: "rgba(255, 255, 255, 1)"}}><path d="M2 12h2a7.986 7.986 0 0 1 2.337-5.663 7.91 7.91 0 0 1 2.542-1.71 8.12 8.12 0 0 1 6.13-.041A2.488 2.488 0 0 0 17.5 7C18.886 7 20 5.886 20 4.5S18.886 2 17.5 2c-.689 0-1.312.276-1.763.725-2.431-.973-5.223-.958-7.635.059a9.928 9.928 0 0 0-3.18 2.139 9.92 9.92 0 0 0-2.14 3.179A10.005 10.005 0 0 0 2 12zm17.373 3.122c-.401.952-.977 1.808-1.71 2.541s-1.589 1.309-2.542 1.71a8.12 8.12 0 0 1-6.13.041A2.488 2.488 0 0 0 6.5 17C5.114 17 4 18.114 4 19.5S5.114 22 6.5 22c.689 0 1.312-.276 1.763-.725A9.965 9.965 0 0 0 12 22a9.983 9.983 0 0 0 9.217-6.102A9.992 9.992 0 0 0 22 12h-2a7.993 7.993 0 0 1-.627 3.122z"></path><path d="M12 7.462c-2.502 0-4.538 2.036-4.538 4.538S9.498 16.538 12 16.538s4.538-2.036 4.538-4.538S14.502 7.462 12 7.462zm0 7.076c-1.399 0-2.538-1.139-2.538-2.538S10.601 9.462 12 9.462s2.538 1.139 2.538 2.538-1.139 2.538-2.538 2.538z"></path></svg>
+                            </i>
+                        </div>
+
+                    </div>
+                    <h2>Compara los precios</h2>
+                </div>
+                <div className="col funciona__col">
+                    <div className="funciona__icon">
+                        <div className="funciona__icon-svg">
+                            <i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" style={{fill: "rgba(255, 255, 255, 1)"}}><path d="m6.516 14.323-1.49 6.452a.998.998 0 0 0 1.529 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822 0L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107zm2.853-4.326a.998.998 0 0 0 .832-.586L12 5.43l1.799 3.981a.998.998 0 0 0 .832.586l3.972.315-3.271 2.944c-.284.256-.397.65-.293 1.018l1.253 4.385-3.736-2.491a.995.995 0 0 0-1.109 0l-3.904 2.603 1.05-4.546a1 1 0 0 0-.276-.94l-3.038-2.962 4.09-.326z"></path></svg>
+                            </i>
+                        </div>
+
+                    </div>
+                    <h2>Escoge tu favorito</h2>
+                </div>
+
             </div>
-            <h1 className="text-center">Como <br /><span>Funciona</span></h1>
-            <p className='funciona__p'>Escribe en la barra de busqueda tu juego favorito, encontrarás una lista de muchos juegos.
-                En la esquina inferior derecha encontrarás un tag que dice el precio, al darle click te aparece más información
-                sobre el juego, como el precio, la plataforma, el género, la fecha de lanzamiento, la descripción, etc.
-            </p>
+            <div>
+                <h3>Y mucho más...</h3>
+                <p>Inicia sesión y guarda tus favoritos</p>
+            </div>
         </div>
     )
 }
