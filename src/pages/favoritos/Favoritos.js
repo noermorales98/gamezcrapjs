7af7e39a6704930b0771032a64f0d79833b9be63
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-03-11 00:21:59 -0500

    usar el componente card

--- src/pages/favoritos/Favoritos.js
+++ src/pages/favoritos/Favoritos.js
@@ -1,4 +1,4 @@
-import Card from './Card';
+import Card from '../../components/games-cards/Card';
 import './Favoritos.css'
 
 function Favoritos() {
@@ -7,11 +7,8 @@ function Favoritos() {
       <div className='favoritos__container'>
         <h1 className='text-white text-center p-4'>Mis favoritos</h1>
         <div className='favoritos__cards'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+          <Card games={{ "title": "Carim Pro", "normalPrice": 12, "salePrice": 21, "thumb": "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/12/halo-infinite-scaled.jpg?fit=2560%2C1637&quality=60&strip=all&ssl=1" }} />
+          <Card games={{ "title": "Carim Pro", "normalPrice": 12, "salePrice": 21, "thumb": "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/12/halo-infinite-scaled.jpg?fit=2560%2C1637&quality=60&strip=all&ssl=1" }} />
         </div>
       </div>
     </div>
